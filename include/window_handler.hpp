d53a8588acb8ab01663fcfa33cb49658b14985bf
Lucas Scialom <lucas.scialom@free.fr>
2018-12-06 16:34:01 +0100

    fixed missing exports

--- include/window_handler.hpp
+++ include/window_handler.hpp
@@ -1,8 +1,5 @@
 #pragma once
 
-//TODO Handle window_handler as a separate lib
-//include vulkan based on macro
-
 #ifdef WINDOW_HANDLER_DLL_EXPORTS
 #define WINDOW_HANDLER_EXPORTS __declspec(dllexport)
 #else
@@ -12,10 +9,10 @@
 #include <stdint.h>
 
 namespace WindowHandler {
-void Init(uint32_t width, uint32_t height);
-bool Update();
-void Shutdown();
+WINDOW_HANDLER_EXPORTS void Init(uint32_t width, uint32_t height);
+WINDOW_HANDLER_EXPORTS bool Update();
+WINDOW_HANDLER_EXPORTS void Shutdown();
 
-void GetFramebufferSize(int *width, int *height);
-void SetFramebufferSizeCallbackSignal(bool *callback);
+WINDOW_HANDLER_EXPORTS void GetFramebufferSize(int *width, int *height);
+WINDOW_HANDLER_EXPORTS void SetFramebufferSizeCallbackSignal(bool *callback);
 } // namespace WindowHandler
